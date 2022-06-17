6336f5317021ad6148f4ede078d7e56d18db02b9
László Kállai <kallailaszlo06@gmail.com>
2022-06-17 21:06:17 +0200

    same text size in the boxes, expire date choosing

--- 2022/06/17/custom.js
+++ 2022/06/17/custom.js
@@ -2,14 +2,27 @@ let bsave = document.querySelector('#save');
 bsave.addEventListener('click', save);
 let bload = document.querySelector('#load');
 bload.addEventListener('click', load);
+
 let title = document.querySelector('#title-input');
 title.addEventListener('input', change);
 let text = document.querySelector('#textarea');
 text.addEventListener('input', change);
 
+let date = document.querySelector('#date');
+date.addEventListener('input', change);
+let time = document.querySelector('#time');
+time.addEventListener('input', change);
+
+let datevar = new Date(Date.now());
+datevar.setFullYear(datevar.getFullYear()+1);
+date.value = datevar.toISOString().substring(0, 10);
+time.value = datevar.toTimeString().substring(0, 8);
+
 function save() {
-    document.cookie = `${title.value}/-/${text.value}`;
+    let setDate = new Date(`${date.value} ${time.value}`);
+    document.cookie = `${title.value}/-/${text.value}; expires=${setDate.toUTCString()}`;
     bsave.style.backgroundColor = "green";
+    console.log(setDate.toUTCString());
 }
 
 function load() {
@@ -23,3 +36,7 @@ function change() {
     bsave.style.backgroundColor = "white";
     bload.style.backgroundColor = "white";
 }
+
+load();
+save();
+change();
