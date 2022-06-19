90548facb82356629cb180947af2ca4630a7b089
László Kállai <kallailaszlo06@gmail.com>
2022-06-19 19:48:31 +0200

    temporary disabled search, and mobile optimalisation in the  main page

--- navbar.js
+++ navbar.js
@@ -1,7 +1,11 @@
 function buttons() {
-    let buttonList = document.querySelectorAll('.button');
+    let menuButton = document.querySelector('.menu-button');
+    menuButton.addEventListener('click', menu);
+    menu();
+
+    let buttonList = document.querySelectorAll('.link');
     for (let i = 0; i < buttonList.length; i++) {
-        buttonList[i].addEventListener('click', clicking);
+        buttonList[i].parentElement.addEventListener('click', clicking);
     }
 
     function clicking(e) {
@@ -13,6 +17,18 @@ function buttons() {
             window.open(a.parentNode.lastChild.textContent);
         }
     }
+
+}
+
+function menu() {
+    let menu = document.querySelector('.menu');
+    // if (menu.style.display == "grid") {
+    //     menu.style.display = "none";
+    // }
+    // else {
+    //     menu.style.display = "grid";
+    // }
+    menu.classList.toggle('display');
 }
 
 function header() {
@@ -27,28 +43,57 @@ function header() {
     div_grid.className = "nav-grid";
     navBar.appendChild(div_grid);
 
-    let div_item = document.createElement('div');
-    div_item.className = "nav-item";
-    // div_item.innerHTML = `<img src="https://kallasz.github.io/favicon.ico" alt="favicon">`;
-    div_item.innerHTML = `<img src="/favicon.ico" alt="favicon">`;
-    div_grid.appendChild(div_item);
+    let menu = document.createElement('div');
+    menu.className = "menu";
+    header.appendChild(menu);
 
-    let div_item2 = document.createElement('div');
-    div_item2.className = "nav-item button nav-button";
-    // div_item2.innerHTML = '<strong>FAVORITES</strong><span class="link">https://kallasz.github.io/2022/05/26</span>';
-    div_item2.innerHTML = '<strong>FAVORITES</strong><span class="link">/2022/05/26</span>';
-    div_grid.appendChild(div_item2);
+    let divItem1 = document.createElement('div');
+    divItem1.className = "nav-item button menu-button";
+    divItem1.innerHTML = '<strong>MENU</strong>';
+    div_grid.appendChild(divItem1);
 
-    let div_item3 = document.createElement('div');
-    div_item3.className = "nav-item button nav-button";
-    div_item3.innerHTML = '<strong>GITHUB REPO</strong><span class="link">https://github.com/kallasz/kallasz.github.io</span>';
-    div_grid.appendChild(div_item3);
+    // let divItem2 = document.createElement('div');
+    // divItem2.className = "nav-item button nav-button";
+    // divItem2.innerHTML = '<strong>FAVORITES</strong><span class="link">/2022/05/26</span>';
+    // div_grid.appendChild(divItem2);
 
-    let div_item4 = document.createElement('div');
-    div_item4.className = "nav-item nav-search";
-    // div_item4.innerHTML = "THERE WILL BE A SEARCH BAR I HOPE";
-    div_item4.innerHTML = '<input type="search" id="search" placeholder="SEARCH">';
-    div_grid.appendChild(div_item4);
+    let divItem3 = document.createElement('div');
+    divItem3.className = "nav-item button nav-button";
+    divItem3.innerHTML = '<strong>GITHUB REPO</strong><span class="link">https://github.com/kallasz/kallasz.github.io</span>';
+    div_grid.appendChild(divItem3);
+
+    let divItem5 = document.createElement('div');
+    divItem5.className = "nav-item text";
+    divItem5.innerHTML = '<strong>KALLASZ.GITHUB.IO</strong>';
+    div_grid.appendChild(divItem5);
+
+    let divItem4 = document.createElement('div');
+    divItem4.className = "nav-item button search-button";
+    divItem4.innerHTML = '<strong>SEARCH</strong>';
+    div_grid.appendChild(divItem4);
+
+    //menu
+    // let divItem2M = document.createElement('div');
+    // divItem2M.className = "nav-item button menu-button";
+    // divItem2M.innerHTML = '<strong>FAVORITES</strong><span class="link">/2022/05/26</span>';
+    // menu.appendChild(divItem2M);
+
+    let divItem3M = document.createElement('div');
+    divItem3M.className = "nav-item button menu-button";
+    divItem3M.innerHTML = '<strong>GITHUB REPO</strong><span class="link">https://github.com/kallasz/kallasz.github.io</span>';
+    menu.appendChild(divItem3M);
+
+    let divItem4M = document.createElement('div');
+    divItem4M.className = "nav-item button menu-button";
+    divItem4M.innerHTML = '<strong>GITHUB REPO</strong><span class="link">https://github.com/kallasz/kallasz.github.io</span>';
+    menu.appendChild(divItem4M);
+
+    // let div_item4 = document.createElement('div');
+    // div_item4.className = "nav-item nav-search";
+    // div_item4.innerHTML = '<input type="search" id="search" placeholder="SEARCH">';
+    // div_grid.appendChild(div_item4);
+    
+    
 }
 
 function main() {
